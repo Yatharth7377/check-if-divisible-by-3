49053947e09066f710fd4538b3444756d547a54e
Yatharth7377 <104558840+Yatharth7377@users.noreply.github.com>
2022-04-28 14:30:54 +0530

    Add files via upload

---
+++ check if divisible by 3.py
@@ -0,0 +1,7 @@
+# A number is division by 3 gives a remainder of 0 it is divisible 
+# If the remainder is 1, it is not divisible
+num = int(input("Enter a number: "))
+if (num % 3) == 0
+print("{0} is divisible by 3".format(num))
+else
+print("{0} is not divisible by 3")
